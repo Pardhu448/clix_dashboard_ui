c9d538a464a32165ef73019a1ee7a9392da92fbc
pardhu448 <pardhuuvce@gmail.com>
2019-09-12 12:59:10 +0530

    added tooltips with more information about legends.

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,4 +1,4 @@
 import { CONFIG } from '../config';
-//const baseUrl = CONFIG.API_BASE_URL;
-const baseUrl = 'http://localhost:5000';
+const baseUrl = CONFIG.API_BASE_URL;
+//const baseUrl = 'http://localhost:5000';
 export default baseUrl;

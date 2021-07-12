dec31b70990c1675e927f67c6b114b2e7eaa4730
yogeshu <yogeshbhavsar1994@gmail.com>
2021-07-12 15:06:49 +0530

    Added user and loggedIn in User Priavate Route Page

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from "react-redux";
 
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect , useHistory} from 'react-router-dom';
 
 var moment = require('moment');
 //console.log(moment(localStorage.getItem('lastlogin')).add(3, 'hours') >= moment())
@@ -12,9 +12,9 @@ const PrivateRoute = ({ view_mode, component: Component, ...rest }) => (
     <Route {...rest} render={props => (
         
         //checking if a user is already stored in local web browser and also loast login in less than 3 hours
-        localStorage.getItem('user') && (moment(localStorage.getItem('lastlogin')).add(3, 'hours') >= moment()) 
+        localStorage.getItem('user') || localStorage.getItem('loggedIn') && (moment(localStorage.getItem('lastlogin')).add(3, 'hours') >= moment()) 
             ? <Component {...props} />
-            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+            :  <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
     )} />
 )
 function mapStateToProps(state){
